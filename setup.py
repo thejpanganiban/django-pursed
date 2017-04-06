9dd7d6fa327d8ecf19ba2657d621aad5499c2eef
Alexandre Varas <alej0varas@gmail.com>
2017-04-06 17:47:16 +0200

    New setup

--- setup.py
+++ setup.py
@@ -1,30 +1,93 @@
-from setuptools import find_packages, setup
+import codecs
 import os
+import re
+
+from setuptools import setup, find_packages
 
 
-README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+###############################################################################
+
+NAME = "django-pursed"
+PACKAGES = find_packages(where="src")
+META_PATH = os.path.join("src", "wallet", "__init__.py")
+KEYWORDS = [""]
+CLASSIFIERS = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "Natural Language :: English",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+INSTALL_REQUIRES = []
+
+###############################################################################
+
+HERE = os.path.abspath(os.path.dirname(__file__))
 
 
-setup(
-    name="django-pursed",
-    version="0.0.1",
-    packages=find_packages(),
-    author="",
-    author_email="",
-    py_modules=['wallet', ],
-    include_package_data=True,
-    license='',  # 'GNU Library or Lesser General Public License (LGPL)',
-    description="",
-    long_description=README,
-    url='',  # 'https://github.com/alej0varas/pypn',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        # 'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
-        'Operating System :: OS Independent',
-        'Topic :: Software Development :: Libraries',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
-    ],
-    keywords='',
+def read(*parts):
+    """
+    Build an absolute path from *parts* and and return the contents of the
+    resulting file.  Assume UTF-8 encoding.
+    """
+    with codecs.open(os.path.join(HERE, *parts), "rb", "utf-8") as f:
+        return f.read()
+
+
+META_FILE = read(META_PATH)
+
+
+def find_meta(meta):
+    """
+    Extract __*meta*__ from META_FILE.
+    """
+    meta_match = re.search(
+        r"^__{meta}__ = ['\"]([^'\"]*)['\"]".format(meta=meta),
+        META_FILE, re.M
+    )
+    if meta_match:
+        return meta_match.group(1)
+    raise RuntimeError("Unable to find __{meta}__ string.".format(meta=meta))
+
+
+VERSION = find_meta("version")
+URI = find_meta("uri")
+LONG = (
+    # read("README.rst") + "\n\n"  # +
+    read("README.md") + "\n\n"  # +
+    # "Release Information\n" +
+    # "===================\n\n" +
+    # re.search("(\d+.\d.\d \(.*?\)\n.*?)\n\n\n----\n\n\n",
+    #           read("CHANGELOG.rst"), re.S).group(1) +
+    # "\n\n`Full changelog " +
+    # "<{uri}en/stable/changelog.html>`_.\n\n".format(uri=URI) +
+    # read("AUTHORS.rst")
 )
+
+
+if __name__ == "__main__":
+    setup(
+        name=NAME,
+        description=find_meta("description"),
+        license=find_meta("license"),
+        url=URI,
+        version=VERSION,
+        author=find_meta("author"),
+        author_email=find_meta("email"),
+        maintainer=find_meta("author"),
+        maintainer_email=find_meta("email"),
+        keywords=KEYWORDS,
+        long_description=LONG,
+        packages=PACKAGES,
+        package_dir={"": "src"},
+        zip_safe=False,
+        classifiers=CLASSIFIERS,
+        install_requires=INSTALL_REQUIRES,
+    )
